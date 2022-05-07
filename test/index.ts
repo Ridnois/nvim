78ef14149dcfc99787949f51add80206a0ed5cf9
Sebastian Altamirano <ridnois@protonmail.com>
2022-05-06 20:10:17 -0400

    comment and uncomment code with gc<c>

--- test/index.ts
+++ test/index.ts
@@ -7,3 +7,13 @@ const weirdString = '//heheh'
 
 console.log(weirdString)
 console.log(greet('World'))
+
+const someFunc = (prop: string) => {
+  // read something
+  const n: number = prop.length;
+  for (let i = 0; i < n; i++) {
+    // print the letter
+    console.log(`Letter ${n} is ${prop[n]}`);
+  }
+  console.log("That's all!");
+}
